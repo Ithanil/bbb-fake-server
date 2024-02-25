ef06ee2d7414f82aa7c02ac93c00699523c91f3a
Jan Kessler <jakessle@uni-mainz.de>
2024-02-25 21:21:39 +0100

    add secret configuration & Rocket-managed storage, sample Rocket.toml and a dummy getMeetings API implementation

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,11 +1,34 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 #[macro_use] extern crate rocket;
+
+use rocket::State;
+use rocket::fairing::AdHoc;
+use rocket::http::RawStr;
+
+struct ApiSecret(String);
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "success"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[get("/getMeetings?<checksum>")]
+fn get_meetings(checksum: &RawStr, api_secret: State<ApiSecret>) -> String {
+    let secret = &api_secret.0;
+    format!("checksum: {checksum}, secret: {secret}")
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/bigbluebutton/api", routes![index, get_meetings])
+        .attach(AdHoc::on_attach("Secret Config", |rocket| {
+            let api_secret = rocket.config()
+                .get_str("api_secret")
+                .unwrap()
+                .to_string();
+
+            Ok(rocket.manage(ApiSecret(api_secret)))
+        }))
+        .launch();
 }
