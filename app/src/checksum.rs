ad6bfb8c3d141851f7634823e28106a735d8ddee
Jan Kessler <jakessle@uni-mainz.de>
2024-02-27 20:55:19 +0100

    cleanup checksum.rs

--- app/src/checksum.rs
+++ app/src/checksum.rs
@@ -1,10 +1,10 @@
 use sha1_smol::Sha1;
 
 pub fn validate_checksum(checksum: &str, api: &str, params: &str, secret: &str) -> bool {
-    let check_string = api.to_string() + &params + secret;
+    let check_string = api.to_string() + params + secret;
     let mut hasher = Sha1::new();
     hasher.update(check_string.as_bytes());
     let check_sum = hasher.digest().to_string();
 
-    return check_sum == checksum;
+    check_sum == checksum
 }
